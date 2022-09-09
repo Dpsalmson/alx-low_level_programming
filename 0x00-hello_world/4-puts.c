9650d2e414b8b6235967cfe1dba3cae2ec6ea673
Dpsalmson <oluwadamilolajonathan1@gmail.com>
2022-09-08 18:40:06 -0700

    Path

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
 /**
- *
- * * main - Printing in c
- *
- * *
- *
- * * Return: Always 0 (It is Successful)
- *
- * */
+ * main - Printing in c
+ * Return: Always 0 (It is Successful)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual
