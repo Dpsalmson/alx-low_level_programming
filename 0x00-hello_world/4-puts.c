ea886139d8cf4ce9ac9a4f7a3c8e31201c8d7bcf
Dpsalmson <oluwadamilolajonathan1@gmail.com>
2022-09-08 18:25:04 -0700

    Put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *
+ * * main - Printing in c
+ *
+ * *
+ *
+ * * Return: Always 0 (It is Successful)
+ *
+ * */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
+
+	return (0);
+
+}
