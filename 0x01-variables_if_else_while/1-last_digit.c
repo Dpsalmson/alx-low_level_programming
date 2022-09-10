308e203e3b666748d6cefce293eb0a2f5605d8ce
Dpsalmson <oluwadamilolajonathan1@gmail.com>
2022-09-09 18:51:11 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ if (l > 5)
 {
         printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
-else if (l == o)
+else if (l == 0)
 {
         printf("Last digit of %d is %d and is 0\n", n, l);
 }
