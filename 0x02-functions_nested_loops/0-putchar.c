28b8a118e0f421b416a5070d4b54a487e5d050ae
Dpsalmson <oluwadamilolajonathan1@gmail.com>
2022-09-13 14:59:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * main - print putchar
  * Return: always 0
  */
-int main(void)
+int  main(void)
 {
 _putchar('_');
 _putchar('p');
@@ -15,5 +15,7 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+
 return (0);
+
 }
