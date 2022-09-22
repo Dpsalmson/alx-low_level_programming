dda0f919f0a8706e9f216f75f2f69900d4793a0c
Dpsalmson <oluwadamilolajonathan1@gmail.com>
2022-09-22 16:20:13 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,12 +8,13 @@
  *
  * Return: destination array
  */
+
 char *_strcat(char *dest, char *src)
 {
 char *ptr = dest;
 
 while (*ptr != 0)
-ptr++;
+*ptr++;
 while (*src != 0)
 *ptr++ = *src++;
 *ptr = 0;
